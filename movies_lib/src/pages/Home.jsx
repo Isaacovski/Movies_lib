23fa3048484471e07e812b036929f9810e0b8364
Isaac gabriel <codebiisaac@gmail.com>
2023-04-15 20:24:11 -0300

    erro na api 401

--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -1,8 +1,30 @@
+import {useState, useEffect} from "react"
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 import React from 'react'
 
 const home = () => {
-  return (<div>home</div>
-  )
+  const [topMovies, setTopMovies] = useState([]);
+ 
+  const getTopratedMovies = async (url) => {
+     const res = await fetch(url);
+     const data = await res.json();
+
+     setTopMovies(data.results);
+  };
+
+  useEffect(()=>{
+
+    const topRateURL = `${moviesURL}top_rates?api_key=${apiKey}`;
+
+  getTopratedMovies(topRateURL);
+  },[]);
+ 
+  return <div>
+    {topMovies && topMovies.map((movie) => {<p>movie.title</p>})}</div>
+  
 }
 
 export default home
